9162051dcc3a9ab74a2a4b292d5fbcfecdbf5d7b
cjncjncc <399496787@qq.com>
2015-01-24 00:27:47 +0800

    finished one

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String TestString ="ABCDE";
+		String ResultString=ZigZagConversion.convert(TestString,4);
+		System.out.println(ResultString);
+	}
+
+}
--- src/ZigZagConversion.java
+++ src/ZigZagConversion.java
@@ -1,11 +1,39 @@
+import java.util.*;
 public class ZigZagConversion {
-	  public String convert(String s, int nRows) {
+	  public static String convert(String s, int nRows) {
+		 if(nRows>1){
 		 int Seg;
 		 int SegLength;
-		 int level;
-		 SegLength=nRows+nRows/2;
+		 String ret="";
+		 SegLength=nRows*2-2;
 		 Seg=s.length()/SegLength;
-		
-		return s;		
+		 Map<Integer,int[]> SameLevelNum=new HashMap<Integer,int[]>();
+		 
+		 for(int i=1;i<nRows+1;i++){
+			 if(i!=1&&i!=nRows){
+				 int []b=new int[2];
+				 b[0]=i;
+				 b[1]=2*nRows-i;
+				 SameLevelNum.put(i, b);
+			 }else{
+				 int []a=new int[1];
+				 a[0]=i;
+				 SameLevelNum.put(i, a);
+			 }
+		 }
+		 for(int i=1;i<=nRows;i++){
+			 for(int j=0;j<Seg+1;j++){
+				 int []Array=SameLevelNum.get(i);
+				 for(int m:Array){
+					 if(SegLength*j+m<s.length()+1){
+						 ret+=s.charAt(SegLength*j+m-1);
+					 }
+				 }
+			 }
+		 }
+		 return ret;		
+		 }else{
+			 return s;
+		 }
 	 }
 }
