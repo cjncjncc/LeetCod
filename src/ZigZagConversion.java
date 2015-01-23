184314875b1b75e7c2c3e8139c00411d2b6759d6
cjncjncc <399496787@qq.com>
2015-01-23 16:32:35 +0800

    zig

---
+++ src/ZigZagConversion.java
@@ -0,0 +1,11 @@
+public class ZigZagConversion {
+	  public String convert(String s, int nRows) {
+		 int Seg;
+		 int SegLength;
+		 int level;
+		 SegLength=nRows+nRows/2;
+		 Seg=s.length()/SegLength;
+		
+		return s;		
+	 }
+}
